8cd2e11d3dbbc17a8217d7a95a80dc0bfa3c5393
Lucía Costamagna <luci3220@hotmail.com>
2023-09-12 22:53:23 -0300

    add Google

---
+++ Google/firebaseConfig.ts
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBIdoZjZ5nNdhBGHYYYtaloW_vOWzAW1Rw",
+  authDomain: "login-github-c2cfa.firebaseapp.com",
+  projectId: "login-github-c2cfa",
+  storageBucket: "login-github-c2cfa.appspot.com",
+  messagingSenderId: "538525404420",
+  appId: "1:538525404420:web:6f3c37635dd8725a2ed451"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
